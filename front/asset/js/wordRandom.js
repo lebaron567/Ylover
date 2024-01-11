12fab227bc1712ae5b64f4f6dc3d8ee1c73fb473
fmaxime <maxime.fuzeau@ynov.com>
2024-01-11 10:10:51 +0100

    :bug: | fonction js word random with fetch | Aide de Tom Saillard

--- front/asset/js/wordRandom.js
+++ front/asset/js/wordRandom.js
@@ -1,26 +1,11 @@
-// const button = document.getElementById("randomWord");
+const button = document.getElementById("randomWord");
 
-async function getRandomWord() {
-  try {
-    // Description: Get a random word from the API
-    const response = await fetch("https://trouve-mot.fr/api/random/1");
-    const data = await response.json();
-    console.log(data[0].name);
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-getRandomWord();
-
-// button.addEventListener("click", () => {
-//   button.innerHTML = `Nombre de clics : ${getRandomWord()}`;
-// });
-
-/*const word = fetch("https://trouve-mot.fr/api/random/1")
+button.addEventListener("click", () => {
+  /* Aide pour la requête fetch par Tom Saillard */
+  fetch("https://trouve-mot.fr/api/random/1")
     .then((response) => response.json())
     .then((data) => {
-      data = data[0].name;
-      return data;
+      console.log(data[0].name);
+      button.innerHTML = `Le mot : ${data[0].name}`;
     });
-  return word;*/
+});
